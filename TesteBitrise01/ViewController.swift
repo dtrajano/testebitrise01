e195b7d2c3f6f1a48683616aa7c3c8e2925c5dde
Diogo Trajano <diogo@trajano.net.br>
2022-01-30 18:53:21 -0300

    Commit inicial

---
+++ TesteBitrise01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TesteBitrise01
+//
+//  Created by Diogo Trajano on 30/01/2022.
+//  Copyright © 2022 Diogo Trajano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
